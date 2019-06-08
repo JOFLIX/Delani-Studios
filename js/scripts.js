a317c1dacf0ecf662ea33864403f9ab12a1c78c3
JOFLIX <joflixooko@outlook.com>
2019-06-08 16:22:11 +0300

    Added the programs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-function myFunction(){
-	var element = document.detElementByClass("p1");
-	element.classList.toggle(".p");
-}
+$(document).ready(function() {
+  $("#design").click(function() {
+   $('.img1').slideToggle(3000);
+    $(".p1").toggle();
+  });
+  $("#development").click(function() {
+    $(".p2, .img2").slideToggle(3000);
+  });
+  $("#productManagement").click(function() {
+    $(".p3, .img3").slideToggle(3000);
+  });
+
+  $('#hoveR').hover(function() {
+    $(this).children(".overlay").fadeToggle(1000, "linear");
+ });
+
+  $("button").click(function(event) {
+    event.preventDefault();
+    var user = document.getElementById('input1').value;
+    alert(user + " we've received your message, thank you for getting in touch with us");
+  });
+});
+
 
 
